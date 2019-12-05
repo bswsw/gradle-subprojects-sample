7c0f9e85335b5cf78e6a7e5d4a11f71974f340d8
baegoony <baegoony@gmail.com>
2019-12-05 14:21:05 +0900

    junit 5 적용

--- subprojects/domain/main/build.gradle.kts
+++ subprojects/domain/main/build.gradle.kts
@@ -5,7 +5,9 @@ dependencies {
     runtimeOnly("mysql:mysql-connector-java")
     kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
     testRuntimeOnly("com.h2database:h2")
 }
 
--- subprojects/domain/main/src/test/kotlin/com/baegoon/domain/main/domain/team/TeamRepositoryTest.kt
+++ subprojects/domain/main/src/test/kotlin/com/baegoon/domain/main/domain/team/TeamRepositoryTest.kt
@@ -2,25 +2,24 @@ package com.baegoon.domain.main.domain.team
 
 import com.baegoon.domain.main.config.MainJpaConfig
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.TestConstructor
 import javax.persistence.EntityNotFoundException
 
-@RunWith(SpringRunner::class)
 @DataJpaTest
 @Import(MainJpaConfig::class)
 @ActiveProfiles("domain-main-test")
-class TeamRepositoryTest {
-
-    @Autowired
-    private lateinit var teamRepository: TeamRepository
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+internal class TeamRepositoryTest(
+    private val teamRepository: TeamRepository
+) {
 
     @Test
+    @DisplayName("이름 검색 테스트")
     fun `이름 검색 테스트`() {
         val team = this.teamRepository.save(Team("테스트팀"))
 
